9b93dfdd18b68a2a03f5a8c8ad8726c3324c558a
Ryan Trimble <mrtrimble2@gmail.com>
2022-10-09 17:57:50 -0400

    added env mode

--- src/components/Token.vue
+++ src/components/Token.vue
@@ -4,16 +4,20 @@ import { onMounted } from "@vue/runtime-core";
 const props = defineProps({
   clientId: String,
   clientSecret: String,
+  mode: String,
 });
 
 const base64 = btoa(props.clientId + ":" + props.clientSecret);
 let oauthToken = localStorage.getItem("oauthToken");
+const redirectUri = props.mode
+  ? "http://localhost:3000/auth/callback"
+  : "https://rimdev-radio.netlify.app/auth/callback";
 
 const endpoint = new URL("https://accounts.spotify.com/api/token");
 
 const authOptions = {
   url: "https://accounts.spotify.com/api/token",
-  body: `grant_type=authorization_code&${oauthToken}&redirect_uri=http://localhost:3000/auth/callback`,
+  body: `grant_type=authorization_code&${oauthToken}&redirect_uri=${redirectUri}`,
   headers: {
     Authorization: `Basic ${base64}`,
     "Content-Type": "application/x-www-form-urlencoded",
@@ -28,18 +32,19 @@ onMounted(() => {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       const now = new Date();
       let time = now.getTime();
       time += 3600 * 1000;
       now.setTime(time);
-      document.cookie = `access_token=${data.access_token}; expires=${now.toUTCString()}; path=/`;
+      document.cookie = `access_token=${
+        data.access_token
+      }; expires=${now.toUTCString()}; path=/`;
       localStorage.clear();
     })
     .then(() => {
-      window.location.replace('/');
+      window.location.replace("/");
     });
-})
+});
 </script>
 
 <template></template>
