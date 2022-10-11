707e4a97c12d182df1ea51d5005fd47d7835771f
Ryan Trimble <mrtrimble2@gmail.com>
2022-10-11 14:01:14 -0400

    forgot file again

---
+++ src/components/RequestForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <form name="request" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="request" />
+    <label for="name"> Name </label>
+    <input type="text" id="name" name="name" />
+    <label for="email"> Email </label>
+    <input type="text" id="email" name="email" />
+    <section class="form-actions">
+      <button type="submit">Request</button>
+    </section>
+  </form>
+</template>
